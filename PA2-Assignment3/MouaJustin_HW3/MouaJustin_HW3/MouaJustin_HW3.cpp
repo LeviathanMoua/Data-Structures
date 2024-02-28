0d7c68d5557aa92b5068e97d255cbf56086cf533
LeviathanMoua <39628304+LeviathanMoua@users.noreply.github.com>
2024-02-28 15:04:02 -0600

    Test

---
+++ PA2-Assignment3/MouaJustin_HW3/MouaJustin_HW3/MouaJustin_HW3.cpp
@@ -0,0 +1,32 @@
+D:\GitHub\Data-Structures\PA2-Assignment3/*
+Program name : MouaJustin_Homework3.cpp
+Author       : Justin Moua
+Professor    : Pu Cong
+Course       : CS 3353 | Data Structures and Algorithms
+Date         : 4 Mayebruary 2024
+Description  : Refer to instructions provided by professor.
+*/
+
+#include <iostream>
+#include <string>
+
+
+
+int main()
+{
+    std::cout << "\n==============================================================\n\nAssignment: Homework #3\nAuthor of Program: Justin moua\nProfessor: Pu Cong\nCourse: CS 3353 | Data Structures and Algorithms\n\n==============================================================\n\n";
+
+    bool boolQuit = true;
+    while (boolQuit) {
+
+        std::string strUsrInp; //Used to store an entire line of input from the user.
+
+        std::cout << "====\nMenu\n====\n\nBinary (0), Octal (1), Hexadecimal (2)\nExit Program (3)\n\nChoose: ";
+        std::getline(std::cin, strUsrInp);
+        std::cout << std::endl;
+
+    }
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
