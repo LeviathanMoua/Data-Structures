88b49ea52a00d59b309d06b004f60e687a4e3143
LeviathanMoua <39628304+LeviathanMoua@users.noreply.github.com>
2024-02-29 12:32:31 -0600

    Misc.

--- PA2-Assignment3/MouaJustin_HW3/MouaJustin_HW3/MouaJustin_HW3.cpp
+++ PA2-Assignment3/MouaJustin_HW3/MouaJustin_HW3/MouaJustin_HW3.cpp
@@ -10,10 +10,48 @@ Description  : Refer to instructions provided by professor.
 #include <iostream>
 #include <string>
 
+//**********************  genListStack.h  *************************
+//     generic stack defined as a doubly linked list
+
+#ifndef LL_STACK
+#define LL_STACK
+
+#include <list>
+
+template<class T>
+class LLStack {
+public:
+    LLStack() {
+    }
+    void clear() {
+        lst.clear();
+    }
+    bool isEmpty() const {
+        return lst.empty();
+    }
+    T& topEl() {
+        return lst.back();
+    }
+    T pop() {
+        T el = lst.back();
+        lst.pop_back();
+        return el;
+    }
+    void push(const T& el) {
+        lst.push_back(el);
+    }
+private:
+    list<T> lst;
+};
+
+#endif
 
 
-int main()
-{
+int main(){
+
+    LLStack<std::string> tempTest;
+
+
     std::cout << "\n==============================================================\n\nAssignment: Homework #3\nAuthor of Program: Justin moua\nProfessor: Pu Cong\nCourse: CS 3353 | Data Structures and Algorithms\n\n==============================================================\n\n";
 
     bool boolQuit = true;
